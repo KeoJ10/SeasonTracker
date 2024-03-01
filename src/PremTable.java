2535e0d942abccc00e1ba83b20115a11a61c3e11
KeoJ10 <keodejong@rcnhelliomare.nl>
2024-03-01 09:08:07 +0100

    cleaned up versie

--- src/PremTable.java
+++ src/PremTable.java
@@ -5,17 +5,17 @@ public class PremTable {
     private int awayGoals;
 
     public static void main(String[] args) {
-        // Creating teams
+
         Team manchesterCity = new PremiereLeague("Manchester City");
         Team liverpool = new PremiereLeague("Liverpool");
         Team chelsea = new PremiereLeague("Chelsea");
 
-        // Creating matches
+
         Match match1 = new PremMatch(manchesterCity, liverpool, 3, 1);
         Match match2 = new PremMatch(liverpool, chelsea, 2, 2);
         Match match3 = new PremMatch(chelsea, manchesterCity, 1, 3);
 
-        // Printing table
+
         premTable(manchesterCity, liverpool, chelsea);
     }
 
